be4cc3b0cea60f1c37fbb80ef3d37b7b1a1fe74f
Tan <tan@intersquad.co>
2018-12-13 09:15:11 +0700

    add default config

--- config/index.js
+++ config/index.js
@@ -3,10 +3,10 @@ require('dotenv').config({ path: './.env' });
 export default {
   env: process.env.NODE_ENV || 'development',
   server: {
-    port: process.env.APP_PORT,
+    port: process.env.APP_PORT || 8080,
   },
   api: {
-    prefix: process.env.API_PREFIX,
-    pageSize: process.env.PAGE_SIZE,
+    prefix: process.env.API_PREFIX || 'https://go-test.epiapi.com/v2',
+    pageSize: process.env.PAGE_SIZE || 10,
   },
 };
